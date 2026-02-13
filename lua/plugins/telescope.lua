40739445b89044d4e273bcec918c5486a98df24f
aman <adsingh434@gmail.com>
2026-02-13 17:52:53 +0530

    pasting in telescope solved

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,14 +1,33 @@
 return {
   {
     "nvim-telescope/telescope.nvim",
-    dependecies = {"nvim-lua/plenary.nvim"},
+    dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
-        local b = require("telescope.builtin")
-        local k = vim.keymap.set
-        k('n', '<leader>ff', b.find_files)
-        k('n', '<leader>fg', b.live_grep)
-        k('n', '<leader>fb', b.buffers)
-    end
+      local telescope = require("telescope")
+      telescope.setup({
+        defaults = {
+          mappings = {
+            i = {
+              ["<C-v>"] = function()
+                local clipboard = vim.fn.getreg("+"):gsub("\n", "")
+                vim.api.nvim_put({ clipboard }, "c", false, true)
+              end,
+            },
+            n = {
+              ["p"] = function()
+                local clipboard = vim.fn.getreg("+"):gsub("\n", "")
+                vim.api.nvim_put({ clipboard }, "c", false, true)
+              end,
+            },
+          },
+        },
+      })
 
-  }
+      local b = require("telescope.builtin")
+      local k = vim.keymap.set
+      k("n", "<leader>ff", b.find_files)
+      k("n", "<leader>fg", b.live_grep)
+      k("n", "<leader>fb", b.buffers)
+    end,
+  },
 }

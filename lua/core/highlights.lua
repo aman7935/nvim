a3ba7519e7e2b60c7252b593a87a3f024c6f3726
aman7935 <adsingh434@gmail.com>
2026-03-06 14:32:50 +0530

    black background

--- lua/core/highlights.lua
+++ lua/core/highlights.lua
@@ -1,29 +1,35 @@
+local function apply_black_background()
+	local black = "#000000"
+
+	-- Core
+	vim.api.nvim_set_hl(0, "Normal", { bg = black })
+	vim.api.nvim_set_hl(0, "NormalNC", { bg = black })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = black })
+	vim.api.nvim_set_hl(0, "FloatBorder", { bg = black })
+	vim.api.nvim_set_hl(0, "SignColumn", { bg = black })
+	vim.api.nvim_set_hl(0, "MsgArea", { bg = black })
+	vim.api.nvim_set_hl(0, "FoldColumn", { bg = black })
+	vim.api.nvim_set_hl(0, "LineNr", { bg = black })
+	vim.api.nvim_set_hl(0, "CursorLineNr", { bg = black })
+
+	-- Neo-tree
+	vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = black })
+	vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = black })
+	vim.api.nvim_set_hl(0, "NeoTreeEndOfBuffer", { bg = black })
+
+	-- Telescope
+	vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = black })
+	vim.api.nvim_set_hl(0, "TelescopePromptNormal", { bg = black })
+	vim.api.nvim_set_hl(0, "TelescopePreviewNormal", { bg = black })
+	vim.api.nvim_set_hl(0, "TelescopeResultsNormal", { bg = black })
+	vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = black })
+	vim.api.nvim_set_hl(0, "TelescopePromptBorder", { bg = black })
+	vim.api.nvim_set_hl(0, "TelescopePreviewBorder", { bg = black })
+	vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { bg = black })
+end
+
 vim.api.nvim_create_autocmd("ColorScheme", {
-    callback = function()
-        -- Core
-        vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
-        vim.api.nvim_set_hl(0, "NormalNC", { bg = "NONE" })
-        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
-        vim.api.nvim_set_hl(0, "FloatBorder", { bg = "NONE" })
-        vim.api.nvim_set_hl(0, "SignColumn", { bg = "NONE" })
-        vim.api.nvim_set_hl(0, "MsgArea", { bg = "NONE" })
-        vim.api.nvim_set_hl(0, "FoldColumn", { bg = "NONE" })
-        vim.api.nvim_set_hl(0, "LineNr", { bg = "NONE" })
-        vim.api.nvim_set_hl(0, "CursorLineNr", { bg = "NONE" })
-
-        -- Neo-tree
-        vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "NONE" })
-        vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "NONE" })
-        vim.api.nvim_set_hl(0, "NeoTreeEndOfBuffer", { bg = "NONE" })
-
-
-        vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "NONE" })
-        vim.api.nvim_set_hl(0, "TelescopePromptNormal", { bg = "NONE" })
-        vim.api.nvim_set_hl(0, "TelescopePreviewNormal", { bg = "NONE" })
-        vim.api.nvim_set_hl(0, "TelescopeResultsNormal", { bg = "NONE" })
-        vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "NONE" })
-        vim.api.nvim_set_hl(0, "TelescopePromptBorder", { bg = "NONE" })
-        vim.api.nvim_set_hl(0, "TelescopePreviewBorder", { bg = "NONE" })
-        vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { bg = "NONE" })
-    end,
+	callback = apply_black_background,
 })
+
+apply_black_background()
